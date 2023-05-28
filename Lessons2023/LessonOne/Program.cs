35b86bd74b3981319549a15f06a229d8ad847843
IvanTushynski <tushinski.ivan@gmail.com>
2023-05-28 23:47:46 +0300

    Add project files.

---
+++ Lessons2023/LessonOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int a, b;
+        a = 2;
+        b = 3;
+        int sum = a + b;
+
+        Console.WriteLine(sum);
+    }
+}
