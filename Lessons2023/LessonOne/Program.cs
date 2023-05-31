8332cd5c770c5b094a5cb0be834625dc761509ad
IvanTushynski <tushinski.ivan@gmail.com>
2023-05-31 22:48:04 +0300

    update

--- Lessons2023/LessonOne/Program.cs
+++ Lessons2023/LessonOne/Program.cs
@@ -13,17 +13,68 @@ internal class Program
         bool isEmployed = false;
         double weight = 78.65;
 
-        Console.WriteLine($"Имя: {name}");
-        Console.WriteLine("Имя: " + name);
-        Console.WriteLine(test.setTest(5));
-        Console.WriteLine(test.getTestInt());
-        Console.WriteLine($"Вес: {weight}");
-        Console.WriteLine($"Работает: {isEmployed}");
+        //Console.WriteLine($"Имя: {name}");
+        //Console.WriteLine("Имя: " + name);
+        //Console.WriteLine(test.setTest(5));
+        //Console.WriteLine(test.getTestInt());
+        //Console.WriteLine($"Вес: {weight}");
+        //Console.WriteLine($"Работает: {isEmployed}");
 
-        for (int i = 1; i < 10; i++)
-        {
-            Console.WriteLine("+Test+");
-            Thread.Sleep(1000);
+        for (int i = 1; i <= 10; i++)
+        { if (i == 0)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("6!");
+            }else if (i == 1)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("5!");
+            }
+            else if (i == 2)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("4!");
+            }
+            else if (i == 3)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("3!");
+            }
+            else if (i == 4)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("2!");
+            }
+            else if (i == 5)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("1!");
+            }
+            else if (i == 6)
+            {
+                Thread.Sleep(2000);
+                Console.Write("Deleting System Files. Please Wait.");
+            }
+            else if (i == 7)
+            {
+                Thread.Sleep(2000);
+                Console.Write(".");
+            }
+            else if (i == 8)
+            {
+                Thread.Sleep(4000);
+                Console.WriteLine(".");
+            }
+            else if (i == 9)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Operation Complete.");
+            }
+            else if (i == 10)
+            {
+                Thread.Sleep(4000);
+                Console.WriteLine("Bye!!!");
+            }
         }
     }
 }
