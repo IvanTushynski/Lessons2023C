fa9c645c5386f3fcdee0f1b45bf21846f4e99b73
IvanTushynski <tushinski.ivan@gmail.com>
2023-05-31 22:17:38 +0300

    update

--- Lessons2023/LessonOne/Program.cs
+++ Lessons2023/LessonOne/Program.cs
@@ -1,19 +1,29 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using Lessons2023.LessonOne;
+using System.Diagnostics.CodeAnalysis;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
 
+        Test test = new Test();
+
+
         string name = "Володя";
-        int age = 33;
         bool isEmployed = false;
         double weight = 78.65;
 
         Console.WriteLine($"Имя: {name}");
         Console.WriteLine("Имя: " + name);
-        Console.WriteLine($"Возраст: {age}");
+        Console.WriteLine(test.setTest(5));
+        Console.WriteLine(test.getTestInt());
         Console.WriteLine($"Вес: {weight}");
         Console.WriteLine($"Работает: {isEmployed}");
+
+        for (int i = 1; i < 10; i++)
+        {
+            Console.WriteLine("+Test+");
+            Thread.Sleep(1000);
+        }
     }
 }
---
+++ Lessons2023/LessonOne/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons2023.LessonOne
+{
+    internal class Test
+    {
+
+       int testInt;
+
+        public int setTest(int testInt)
+        {
+            return testInt;
+        }
+
+        public int getTestInt() {
+        
+            this.testInt = 2;
+            return testInt;
+        }
+
+    }
+}
