c984397948615466be28ea66305a9a8b6bd5f0e3
IvanTushynski <tushinski.ivan@gmail.com>
2023-06-05 09:24:43 +0300

    update

--- Lessons2023/LessonOne/One/MainProg.cs
+++ Lessons2023/LessonOne/One/MainProg.cs
@@ -11,12 +11,31 @@ internal class MainProg
         double weight = 3.3;
         decimal testOneDecimal = 22345.345634m;
 
-        int[] terstMassive = { 1, 2, 3};
+
+
+
+        int sum = 1700 * 3;
+        Console.WriteLine($"XRP SUM: {sum}");
+        Console.WriteLine($"-----------------------------------------");
+
+        //-------------------------------MASSIVE-ONE-------------------------------------
+        int[] terstMassive = {1, 2, 3};
         string[] testMassive2 = {"asd", "asdasd", "asdasd"};
 
+        //-------------------------------MASSIVE-DOUBLE-------------------------------------
         int[,] testTeoMerMassive = new int[2,3];
         testTeoMerMassive[1, 2] = 3;
         Console.WriteLine($"Massive1.2: {testTeoMerMassive[1, 2]}");
+
+
+        int[,] array3D = new int[,] { 
+            { 1, 2, 3 }, 
+            { 4, 5, 6 } };
+
+
+        Console.WriteLine($"test massive3d {array3D[1,1]}");
+
+
 
         Console.WriteLine(terstMassive.GetType());
         Console.WriteLine(testMassive2.GetType());
@@ -26,7 +45,7 @@ internal class MainProg
         Console.WriteLine($"Вес: {weight}");
         Console.WriteLine($"Test Decimal: {testOneDecimal}");
 
-        //---------------------------------------------------------------------
+        //-----------------------------CALL--METHODS--------------------------------------
 
         Test test = new Test();
 
