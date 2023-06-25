9afd6e81c9e68c6e0654810b17d9a98684443630
IvanTushynski <tushinski.ivan@gmail.com>
2023-06-25 19:15:43 +0300

    upadate

--- Lessons2023/LessonOne/Two/Lists.cs
+++ Lessons2023/LessonOne/Two/Lists.cs
@@ -13,10 +13,19 @@ namespace Lessons2023.LessonOne.Two
 
         public void ListPeoples()
         {
+            //add new list directories
+            peoples.Add("IVAN");
+            peoples.Add("ANDREI");
+            peoples.Add("GLEB");
+            peoples.Add("EHOR");
+
             Console.WriteLine($"Name one: {peoples[1]}");
             Console.WriteLine($"Name two: {peoples[0]}");
             Console.WriteLine($"Name three: {peoples[2]}");
             Console.WriteLine($"Name four: {peoples[3]}");
+            Console.WriteLine($"Name five: {peoples[4]}");
+            Console.WriteLine($"Name added six: {peoples[6]}");
+            Console.WriteLine($"Names List Count: {peoples.Count}");
         }
 
     };
